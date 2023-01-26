c1f868807fb473194d89323b16b983ee9adb7398
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2023-01-25 19:06:22 -0800

    Use consts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+const STARTING_MISSILES: i32 = 8;
+const READY: i32 = 2;
+
 fn main() {
-    let missiles = 8;
-    let ready = 2;
+    let missiles = STARTING_MISSILES;
+    let ready = READY;
     println!("Firing {} of my {} missiles...", ready, missiles);
 }
