72f3de10018cb7b8ad0f151bc6a9fadcd28fc814
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2023-01-28 01:56:40 -0800

    Meh

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-const STARTING_MISSILES: i8 = 8;
-const READY: i8 = 2;
 
-fn main() {
-    let missiles = STARTING_MISSILES;
-    let ready = READY;
-    println!("Firing {} of my {} missiles...", ready, missiles);
-}
+fn main(){
+    let a:[i32;5] = [3;5];  // Declares and initializes (read fills) an array of size 5, with 3
+    let b:[i32;10] = [1,2,3,4,5,6,7,8,9,10]; // This also works
+    println!("{:?} and {:?}",a,b);  // The ':?' Adds complex printing.
+    
+    }
